dae4176c93800c309cd5bb67c6e8992f7e62dee2
Andrii Lutsiak <timechain04@gmail.com>
2023-07-06 16:46:37 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import './App.css';
+import { Profile } from './profile/Profile';
+import user from '../data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
     </div>
   );
-};
+}
